f72617b46d6a791669cd93c16cb0f741d700be0e
Yusaiyin <yusaiyin89@gmail.com>
2024-02-02 14:43:18 +0100

    Event name styling

--- src/script/class.js
+++ src/script/class.js
@@ -1,98 +1,86 @@
 export class BigEvent {
-constructor(header, description, img) {
+  constructor(header, description, img) {
     this.header = header;
     this.description = description;
     this.img = img;
-    this.createStartingElement()
-}
-createStartingElement = () => {
-    this.prepareStaticHtml()
-    this.attachStaticEventListeners()
-    this.updateDynamicValues()  
-}
+    this.createStartingElement();
+  }
+  createStartingElement = () => {
+    this.prepareStaticHtml();
+    this.attachStaticEventListeners();
+    this.updateDynamicValues();
+  };
 
-prepareStaticHtml = () => {
+  prepareStaticHtml = () => {
+    const majorDiv = document.querySelector(".main");
 
-    const majorDiv = document.querySelector('.main')
+    this.mainDiv = document.createElement("div");
+    this.mainDiv.classList.add("big-event-container");
 
-    this.mainDiv = document.createElement('div')
-    this.mainDiv.classList.add('big-event-container')
+    this.header = document.createElement("h2");
+    this.header.classList.add("big-event-header");
 
-    this.header = document.createElement('h2')
-    this.header.classList.add('big-event-header')
+    this.description = document.createElement("p");
+    this.description.classList.add("big-event-description");
 
-    this.description = document.createElement('p')
-    this.description.classList.add('big-event-description')
+    this.button = document.createElement("button");
+    this.button.classList.add("big-event-button");
+    this.button.textContent = "Register";
 
-    this.button = document.createElement('button')
-    this.button.classList.add('big-event-button')
-    this.button.textContent = 'Register'
+    this.img = document.createElement("img");
+    this.img.classList.add("big-event-img");
+    this.img.src = `${this.img}`;
+    this.img.alt = "Photo of main event";
 
-    this.img = document.createElement('img')
-    this.img.classList.add('big-event-img')
-    this.img.src = `${this.img}`
-    this.img.alt = 'Photo of main event'
-    
-    majorDiv.appendChild(this.mainDiv)
-    this.mainDiv.appendChild(this.header)
-    this.mainDiv.appendChild(this.img)
-    this.mainDiv.appendChild(this.description)
-    this.mainDiv.appendChild(this.button)
-}
+    majorDiv.appendChild(this.mainDiv);
+    this.mainDiv.appendChild(this.header);
+    this.mainDiv.appendChild(this.img);
+    this.mainDiv.appendChild(this.description);
+    this.mainDiv.appendChild(this.button);
+  };
 
-attachStaticEventListeners = () => {
+  attachStaticEventListeners = () => {};
 
-}
-
-updateDynamicValues = () => {
-
-}
-
-
+  updateDynamicValues = () => {};
 }
 
 export class SmallEvent {
-    constructor(header) {
-        this.header = header;
-        this.createStartingElement()
-    }
-    createStartingElement = () => {
-        this.prepareStaticHtml()
-        this.attachStaticEventListeners()
-        this.updateDynamicValues()  
-    }
-    
-    prepareStaticHtml = () => {
-    const majorDiv = document.querySelector('.main')
+  constructor(header) {
+    this.header = header;
+    this.createStartingElement();
+  }
+  createStartingElement = () => {
+    this.prepareStaticHtml();
+    this.attachStaticEventListeners();
+    this.updateDynamicValues();
+  };
 
-    this.smallDiv = document.createElement('div')
-    this.smallDiv.classList.add('small-event-container')
+  prepareStaticHtml = () => {
+    const majorDiv = document.querySelector(".main");
 
-    this.smallDivBox = document.createElement('div')
-    this.smallDivBox.classList.add('small-event-box')
+    this.smallDiv = document.createElement("div");
+    this.smallDiv.classList.add("small-event-container");
 
-    this.header = document.createElement('h2')
-    this.header.classList.add('small-div-header')
+    this.smallDivBox = document.createElement("div");
+    this.smallDivBox.classList.add("small-event-box");
 
-    this.button = document.createElement('button')
-    this.button.textContent = 'More'
-    this.classList.add('small-event-button')
+    this.header = document.createElement("h2");
+    this.header.classList.add("small-div-header");
 
-    majorDiv.appendChild(this.smallDiv)
-    this.smallDiv.appendChild(this.smallDivBox)
-    this.smallDivBox.appendChild(this.header)
-    this.smallDivBox.appendChild(this.button)
-    }
-    
-    attachStaticEventListeners = () => {
-        this.button = document.querySelector('.small-event-button')
-        this.button.addEventListener('click', () => {
-        
-        })
-    }
-    
-    updateDynamicValues = () => {
-    
-    }
+    this.button = document.createElement("button");
+    this.button.textContent = "More";
+    this.classList.add("small-event-button");
 
+    majorDiv.appendChild(this.smallDiv);
+    this.smallDiv.appendChild(this.smallDivBox);
+    this.smallDivBox.appendChild(this.header);
+    this.smallDivBox.appendChild(this.button);
+  };
+
+  attachStaticEventListeners = () => {
+    this.button = document.querySelector(".small-event-button");
+    this.button.addEventListener("click", () => {});
+  };
+
+  updateDynamicValues = () => {};
 }
